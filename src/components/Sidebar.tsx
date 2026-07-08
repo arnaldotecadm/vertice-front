b4342e6b260d073ad91f74c092543b670b81116c
arnaldo.bezerra <arnaldo.bezerra@kuehne-nagel.com>
2026-07-08 10:57:08 +0100

    Add sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import './Sidebar.css'
+
+// Placeholder left-hand panel. Menu items will be added here later.
+function Sidebar() {
+  return (
+    <aside id="sidebar">
+      <h2>Menu</h2>
+      <p className="sidebar-placeholder">Coming soon</p>
+    </aside>
+  )
+}
+
+export default Sidebar
